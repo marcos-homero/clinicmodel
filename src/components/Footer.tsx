cda58f99c2dfef05856d79cba5c9ec73429d7f82
Marcos Homero <marcosfinanceiro1996@gmail.com>
2024-09-26 10:08:37 -0300

    New Att 26/09/2024

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import React from "react";
-import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import {
+  FaClinicMedical,
+  FaFacebook,
+  FaInstagram,
+  FaWhatsapp,
+} from "react-icons/fa";
 
 const Footer = () => {
   const links = [
@@ -13,20 +18,61 @@ const Footer = () => {
   return (
     <div
       id="footer"
-      className="bg-[#031432] w-full flex items-center py-16 justify-center"
+      className="bg-[#0f213e] w-full flex flex-col items-center justify-center"
     >
-      <div className="flex flex-col w-full px-6 max-w-[1300px] gap-4">
-        <div className="flex flex-col lg:flex-row lg:justify-between w-full ">
-          <div className="flex flex-col gap-4 max-lg:items-center">
-            <h2 className="text-white text-4xl font-semibold">Intelimed</h2>
+      <div className="flex flex-col w-full px-6 py-16 max-w-[1300px] h-[75%] gap-4">
+        <div className="flex flex-col lg:flex-row lg:justify-between w-full gap-16 ">
+          <div className="flex flex-col gap-8 lg:flex-row w-full">
+            <div className="flex flex-col gap-4 max-lg:items-center">
+              <h2 className="text-white font-normal">Paciente:</h2>
+              <div className="flex flex-col gap-2 max-lg:items-center">
+                {links.map((item, index) => (
+                  <p
+                    className="text-sky-200 text-sm hover:underline cursor-pointer"
+                    key={index}
+                  >
+                    {item.name}
+                  </p>
+                ))}
+              </div>
+            </div>
+            <div className="flex flex-col gap-4 max-lg:items-center">
+              <h2 className="text-white font-normal">Sobre nós:</h2>
+              <div className="flex flex-col gap-2 max-lg:items-center">
+                {links.map((item, index) => (
+                  <p
+                    className="text-sky-200  hover:underline text-sm cursor-pointer"
+                    key={index}
+                  >
+                    {item.name}
+                  </p>
+                ))}
+              </div>
+            </div>
+            <div className="flex flex-col gap-4 max-lg:items-center">
+              <h2 className="text-white font-normal">Informações:</h2>
+              <div className="flex flex-col gap-2 max-lg:items-center">
+                {links.map((item, index) => (
+                  <p
+                    className="text-sky-200 text-sm hover:underline cursor-pointer"
+                    key={index}
+                  >
+                    {item.name}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4 items-center">
+            <h1 className="text-white text-4xl font-semibold">
+              <div className="flex flex-col justify-around items-center w-fit">
+                <FaClinicMedical size={64} className="text-white" />
+                <h1 className="text-white text-xl font-bold">Intelimed</h1>
+              </div>
+            </h1>
             <div className="flex flex-col max-lg:items-center gap-2">
-              <p className="text-white text-sm text-start">
-                Endereço: Rua Coronel Fernando Prestes 144, Centro, Itapetininga
-              </p>
-              <p className="text-white text-sm text-start">
-                Endereço: Rua Coronel Fernando Prestes 144, Centro, Sorocaba
-              </p>
-              <p className="text-white text-sm">Contato: (15) 3537-0431</p>
+              <p className="text-white font-semibold">Rede Sociais</p>
             </div>
             <div className="flex gap-4">
               <Link
@@ -61,22 +107,9 @@ const Footer = () => {
               </Link>
             </div>
           </div>
-          <div className="flex flex-col gap-4 max-lg:items-center">
-            <h2 className="text-white text-2xl font-semibold">Informações:</h2>
-            <div className="flex flex-col gap-2 max-lg:items-center">
-              {links.map((item, index) => (
-                <p
-                  className="text-white text-end hover:text-primary cursor-pointer"
-                  key={index}
-                >
-                  {item.name}
-                </p>
-              ))}
-            </div>
-          </div>
         </div>
-
-        <span className="h-[2px] bg-white w-full"></span>
+      </div>
+      <div className="w-full h-[25%] py-16 bg-[#152e56]">
         <p className="text-white text-center">
           © COPYRIGHT 2024 - MARKOALA NETWORK S.A. TODOS OS DIREITOS RESERVADOS.
         </p>
